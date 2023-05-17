ab5deb0a1e4cbb7ec81f952a24aba6d6c1e7736b
nabezokodaikon <nabezoko.daikon@gmail.com>
2023-05-18 01:25:01 +0900

    Clean

--- lua/translate/util.lua
+++ lua/translate/util.lua
@@ -45,7 +45,7 @@ M.get_visual_selection = function()
   local n_lines = math.abs(s_end[2] - s_start[2]) + 1
   local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
   if lines[1] == nil then
-    return ''
+    return ""
   end
   lines[1] = string.sub(lines[1], s_start[3], -1)
   if n_lines == 1 then
