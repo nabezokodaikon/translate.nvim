93c4a7a5fc586545322f0ea56cd6d543ff5349b6
nabezokodaikon <nabezoko.daikon@gmail.com>
2023-05-17 20:07:54 +0900

    Add run_command

--- lua/translate/util.lua
+++ lua/translate/util.lua
@@ -1,10 +1,47 @@
 local M = {}
 
+local function remove_last_line(str)
+  local letters = {}
+  for let in string.gmatch(str, ".") do
+    table.insert(letters, let)
+  end
+
+  local i = #letters
+  while i >= 0 do
+    if letters[i] == "\n" then
+      letters[i] = nil
+      break
+    end
+    letters[i] = nil
+    i = i - 1
+  end
+  return table.concat(letters)
+end
+
+M.unpack_args = function(args_string)
+  if args_string == nil then
+    return {}
+  end
+
+  local t = {}
+  local i = 0
+
+  for s in string.gmatch(args_string, "([^%s]+)") do
+    t[i] = s
+    i = i + 1
+  end
+
+  return t
+end
+
 M.get_visual_selection = function()
   local s_start = vim.fn.getpos("'<")
   local s_end = vim.fn.getpos("'>")
   local n_lines = math.abs(s_end[2] - s_start[2]) + 1
   local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
+  if lines[1] == nil then
+    return ''
+  end
   lines[1] = string.sub(lines[1], s_start[3], -1)
   if n_lines == 1 then
     lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
@@ -14,9 +51,9 @@ M.get_visual_selection = function()
   return table.concat(lines, '\n')
 end
 
-M.ja_2_en = function(src_text)
-  local handle = io.popen("trans -b -show-original n -sl=ja -tl=en '" ..src_text.. "'")
-  local dest_text = handle:read("*a")
+M.run_command = function(src_text, sl, tl)
+  local handle = io.popen("trans -b -show-original n -sl=" ..sl.. " -tl=" ..tl.. " '" ..src_text.. "'")
+  local dest_text = remove_last_line(handle:read("*a"))
   return dest_text;
 end
 
--- plugin/translate.lua
+++ plugin/translate.lua
@@ -5,15 +5,15 @@ end
 vim.g.loaded_translate = true
 
 vim.api.nvim_create_user_command(
-  "TransLateJa2En",
-  function()
+  "TranslateJa2En",
+  function(opts)
     local util = require("translate.util")
-    local ja_text = util.get_visual_selection() 
-    local en_text = util.ja_2_en(ja_text)
-    print(en_text)
+    local args = util.unpack_args(opts.args)
+    local src_text = util.get_visual_selection() 
+    local dest_text = util.run_command(src_text, args[0], args[1])
+    print(dest_text)
   end,
   {
-    nargs = 0,
+    nargs = "*",
   })
-
 
